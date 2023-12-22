aed4b771e98f9b893c8561f87f520a0308fb2c36
Oswin <129711775+OswinJTC@users.noreply.github.com>
2023-12-22 22:35:29 +0800

    Update Top5Movies.js

--- src/components/my_vote/top_5_movies/Top5Movies.js
+++ src/components/my_vote/top_5_movies/Top5Movies.js
@@ -186,13 +186,13 @@ const Top5Movies = ({ movies }) => {
                 <Link to="/Top3Movies">最爛劇場版 top 3</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/suspense-movies">最懸疑</Link>
+                <Link to="/PleaseWait">最懸疑</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/exciting-movies">最刺激</Link>
+                <Link to="/PleaseWait">最刺激</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/artistic-movies">最有藝術</Link>
+                <Link to="/PleaseWait">最有藝術</Link>
               </li>
             </ul>
           </div>
@@ -200,13 +200,13 @@ const Top5Movies = ({ movies }) => {
             <h5 className="text-center" style={{ marginBottom: "20px" }}>角色</h5>
             <ul className="list-group">
               <li className="list-group-item">
-                <Link to="/favorite-characters">喜歡的角色</Link>
+                <Link to="/PleaseWait">喜歡的角色</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/hated-characters">討厭的角色</Link>
+                <Link to="/PleaseWait">討厭的角色</Link>
               </li>
               <li className="list-group-item">
-                <Link to="/best-couples">最佳情侶黨</Link>
+                <Link to="/PleaseWait">最佳情侶黨</Link>
               </li>
             </ul>
           </div>
